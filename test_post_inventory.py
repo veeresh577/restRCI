a6c45a696ef3336bee89c45596d24a58f6c41c09
Veeresh <veereshsm577@gmail.com>
2020-02-27 19:20:49 +0530

    adding negative test cases

--- test_post_inventory.py
+++ test_post_inventory.py
@@ -4,17 +4,20 @@ from RestRCI import *
 
 log.info("\n------  test_post_inventory REST RCI api test ----------\n")
 
-# reader = config.get("inventory","command_Driven")
-# reader =reader.split(character)
-reader = {"Cmd":"_GetTags","_StopCondition": 1, "_StopValue":1}
+reader = config.get("inventory","command_Driven")
+reader =reader.split(character)
+# reader=[str({"Cmd":"_GetTags","_StopCondition": 3, "_StopValue":6})]
+# print(reader)
+# print(type(reader))
 
 @pytest.mark.parametrize('command',reader)
 def test_post_inventory(api, command):
-    '''Inventory api testing'''
-    time.sleep(2)
     response = requests.post(api.url,data=command)
+    print(command)
     json_resp = json.loads(response.text)
-    time.sleep(2)
-    log.info("command :) {} \n response :) {}\n".format(command,json_resp))
-    assert response.status_code == 200 and json_resp["Tags"][0]["ErrID"] == 0
+
+    print(json_resp)
+    log.info("command :) {} \n \tresponse :) {}\n".format(command,json_resp))
+    # error =api.GetERROR(json_resp)
+    assert response.status_code == 200 and json_resp["ErrID"] == 0
 
